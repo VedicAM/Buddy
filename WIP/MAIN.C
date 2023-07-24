e3d2e0332d27b56ee951774200ac5dfc96f7eaa8
YellowBoyYams <99448454+YellowBoyYams@users.noreply.github.com>
2023-07-24 14:29:38 -0400

    Vertices now get connected by triangles

--- WIP/MAIN.C
+++ WIP/MAIN.C
@@ -1,33 +1,57 @@
-
 #include <conio.h>
 #include <stdio.h>
 #include <math.h>
 
 #include "DRAWING.H"
+#include "UTIL.H"
 #include "MATMATH.H"
 #include "TRANS.H"
 
 int kc = 0;
 
-Vec3D points[] = {{25, 25, 25}, {25, 25, 50},
-		  {50, 25, 25}, {25, 50, 25},
-		  {25, 50, 50}, {50, 25, 50},
-		  {50, 50, 25}, {50, 50, 50}};
+Vec3D points[] = {{25, 25, 50}, {50, 25, 50},
+		  {25, 50, 50}, {50, 50, 50},
+		  {25, 25, 25}, {50, 25, 25},
+		  {25, 50, 25}, {50, 50, 25}};
 
-float projection[2][3] = {{1/0.5, 0, 0},
-			{0, 1/0.5, 0}};
+int indices[] = {
+	 2, 6, 7,
+	 2, 3, 7,
+	 0, 4, 5,
+	 0, 1, 5,
+	 0, 2, 6,
+	 0, 4, 6,
+	 1, 3, 7,
+	 1, 5, 7,
+	 0, 2, 3,
+	 0, 1, 3,
+	 4, 6, 7,
+	 4, 5, 7
+};
+
+int projectedPointsX[1024];
+int projectedPointsY[1024];
+
+float projection[2][3] = {{1/0.35, 0, 0},
+			{0, 1/0.35, 0}};
 
 void render(){
- int i;
+ int i; int j;
+ float angle = 20;
  Vec3D projected2D;
  Vec3D rotated;
 
  for(i = 0; i < sizeof(points)/sizeof(points[0]); i++){
-  rotated = rotateZ(points[i], 20);
-  rotated = rotateX(rotated, 20);
+  rotated = rotateZ(points[i], angle);
+  rotated = rotateX(rotated, angle);
   projected2D = matmul2x3(projection, rotated);
 
-  SETPIX(projected2D.x, projected2D.y, WHITE);
+  projectedPointsX[i] = projected2D.x;
+  projectedPointsY[i] = projected2D.y;
+ }
+
+ for(j = 0; j < sizeof(indices) / sizeof(indices[0]); j+=3){
+  wireTriangle(projectedPointsX[indices[j]], projectedPointsY[indices[j]], projectedPointsX[indices[j+1]], projectedPointsY[indices[j+1]], projectedPointsX[indices[j+2]], projectedPointsY[indices[j+2]], WHITE);
  }
 }
 
@@ -42,11 +66,9 @@ int main(){
 
  while(kc != 0x1b){
   kc = getch();
-
   /* Game Loop */
-
-
  }
+
  setMode(TEXT_MODE);
  return 0;
 }
